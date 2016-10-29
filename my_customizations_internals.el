4212ae67636aa9cfe85b39810648fa96db387c22
Mohamed Haseeb <mohamedaziz52@gmail.com>
2016-10-29 07:05:48 +0200

    org-indent-mode enabled by default

--- my_customizations_internals.el
+++ my_customizations_internals.el
@@ -15,3 +15,5 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(add-hook 'org-mode-hook 'org-indent-mode)
